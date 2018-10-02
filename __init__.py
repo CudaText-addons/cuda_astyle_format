8f101fcaebe9bdc4be1e5892cd1950bd7cc08c16
Alexey-t <support@uvviewsoft.com>
2018-10-02 10:50:51 +0300

    support Unix

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
-from cudatext import *
 import sys
 import os
 import shutil
-
-if os.name!='nt':
-    msg_box('Plugin supports Win32 only', MB_OK)
-    raise Exception('')
-    
-from . import pyastyle
+from cudatext import *
 from . import format_proc
+
+if os.name=='nt':
+    from . import pyastyle
+else:
+    try:
+        import pyastyle
+    except:
+        pyastyle = None
+        msg_box('For "AStyle Format" plugin, you need to install "pyastyle" Python module in OS. Run:\nsudo pip3 install pyastyle',
+            MB_OK+MB_ICONERROR)
+
 
 format_proc.INI = 'cuda_astyle_format.cfg'
 format_proc.MSG = '[AStyle Format] '
